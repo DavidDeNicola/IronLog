b7cb2e65dfd57a8e1821cc4b4ecead2f72a7ccb1
david_deNicola <david_de_nicola@icloud.com>
2026-08-29 15:23:20 +0200

    chore: setup monorepo backend Spring Boot + frontend Angular

---
+++ ironlog-backend/src/main/java/org/ironlog/app/IronlogApplication.java
@@ -0,0 +1,13 @@
+package org.ironlog.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronlogApplication.class, args);
+	}
+
+}
---
+++ ironlog-backend/src/test/java/org/ironlog/app/IronlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ironlog.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
